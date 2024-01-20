0b16ea5bd6a09cb37dd2cee7a73dd1108bd56114
sourabh-11 <sourabhjangid2002@gmail.com>
2024-01-20 22:55:36 +0530

    selectionsort done

---
+++ mergesort.cpp
@@ -0,0 +1,107 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void merge(int arr, int left, int mid, int right) {
+    int n1 = mid - left + 1;
+    int n2 = right - mid;
+
+    // Create temporary arrays
+    vector<int> leftArray(n1);
+    vector<int> rightArray(n2);
+
+    // Copy data to temporary arrays leftArray[] and rightArray[]
+    for (int i = 0; i < n1; i++) {
+        leftArray[i] = arr[left + i];
+    }
+    for (int j = 0; j < n2; j++) {
+        rightArray[j] = arr[mid + 1 + j];
+    }
+
+    // Merge the temporary arrays back into arr[left..right]
+    int i = 0;  // Initial index of left subarray
+    int j = 0;  // Initial index of right subarray
+    int k = left;  // Initial index of merged subarray
+
+    while (i < n1 && j < n2) {
+        if (leftArray[i] <= rightArray[j]) {
+            arr[k] = leftArray[i];
+            i++;
+        } else {
+            arr[k] = rightArray[j];
+            j++;
+        }
+        k++;
+    }
+
+    // Copy the remaining elements of leftArray[], if there are any
+    while (i < n1) {
+        arr[k] = leftArray[i];
+        i++;
+        k++;
+    }
+
+    // Copy the remaining elements of rightArray[], if there are any
+    while (j < n2) {
+        arr[k] = rightArray[j];
+        j++;
+        k++;
+    }
+}
+
+void mergeSort(int  arr, int left, int right) {
+    if (left < right) {
+        // Same as (left+right)/2, but avoids overflow for large left and right
+        int mid = left + (right - left) / 2;
+
+        // Sort first and second halves
+        mergeSort(arr, left, mid);
+        mergeSort(arr, mid + 1, right);
+
+        // Merge the sorted halves
+        merge(arr, left, mid, right);
+    }
+}
+void printArray(int arr[],int n)
+{
+	for(int i=0;i<n;i++)
+	{
+		cout<<arr[i]<<" ";
+	}
+}
+
+
+int main() {
+   int  arr = {12, 11, 13, 5, 6, 7};
+    int n = sizeof(arr)/sizeof(arr[0]);
+
+    cout << "Original array: ";
+    printArray(arr,n);
+
+    int arrSize = arr.size();
+
+    // Perform merge sort
+    mergeSort(arr, 0, arrSize - 1);
+
+   cout << "Sorted array: ";
+    printArray(arr);
+
+    return 0;
+}
+
