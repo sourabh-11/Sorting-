38a1b1e6213e9fcea3f24f1453f3208a0e6a2639
sourabh-11 <sourabhjangid2002@gmail.com>
2024-01-25 14:49:25 +0530

    also pattern done

--- diamand pattern odd.cpp
+++ diamand pattern odd.cpp
@@ -4,38 +4,34 @@ int main()
 {   int n;
     cout<<"enter the number of rows"<<endl;
     cin>>n;
-    int space = n-1;
-    for(int i = 1;i<=n;i++)
-    {
-    	for(int j = 1;j<=space;j++)
-    	{
-    		cout<<" ";
+     int space = n-1;
+      for(int i = 1;i<=n;i++)
+      {
+      	for(int j = 1;j<=space;j++)
+      	{
+      		cout<<" ";
 		}
-		for(int j = 1;j<=i;j++)
+		for(int j= 1;j<=(2*i-1);j++)
 		{
-			cout<<"* ";
-			
+			cout<<"*";
 		}
 		cout<<endl;
 		space--;
-	}
-	space = 0;
-	for(int i = n;i>=1;i--)
-	{
-		for(int j = 0;j<space;j++)
-		{
-			cout<<" ";
+	  }
+	  space = 0;
+	  for(int i = n;i>=1;i--)
+      {
+      	for(int j = 1;j<=space;j++)
+      	{
+      		cout<<" ";
 		}
-		for(int j=1;j<=i;j++)
+		for(int j= 1;j<=(2*i-1);j++)
 		{
-			cout<<"* ";
+			cout<<"*";
 		}
 		cout<<endl;
 		space++;
-	}
-	
-	
-	
+	  }
 	
 	
 	
